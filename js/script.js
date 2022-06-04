6dc51b90dc890bc3e9dec2804de843baf6b80b27
Maria Zakharova <zakharovamary@gmail.com>
2022-06-04 15:04:28 +0200

    prefinal

--- js/script.js
+++ js/script.js
@@ -4,34 +4,98 @@ console.table(plans);
 function updateHTML(arr) {
     document.getElementById("tasklist").innerHTML = "";
     for (let data of arr) {
-        document.getElementById("tasklist").innerHTML += `
-    <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-  
+        if (data.priority <= 1) {
+            document.getElementById("tasklist").innerHTML += `
+
+<div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
     <div class="card" ">
-    <div class="taskBtn">
-    <button type="button" class="btn btn-info btn-sm text-white">Task</button>
-    <p><i class='fa fa-bookmark-o'></i>&nbsp<i class='fa fa-ellipsis-v'></i></p>
+        <div class="taskBtn">
+            <button type="button" class="btn btn-info btn-sm text-white">Task</button>
+            <p><i class='fa fa-bookmark-o'></i>&nbsp<i class='fa fa-ellipsis-v'></i></p>
+        </div>
+            <img src="${data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${data.task}</h5>
+                <p class="card-text">${data.about}.</p>
+                <hr>
+                <div class="priority">
+                <p><i class='fa fa-warning'></i> Priority level<a class="btn-clicks btn btn-success"><span class="result">${data.priority}</span></a></p>
+                <p><i class='fa fa-calendar-times-o'></i> Deadline: ${data.deadline}</p>
+                <hr>
+                </div>
+                <div class="buttons">
+                    <button type="button" class="btn btn-danger"><i class='fa fa-trash'></i> Delete</button>
+                    <button type="button" class="btn btn-success"><i class='fa fa-check-circle'></i> Done</button>
+                </div>
+            </div>
     </div>
-  <img src="${data.image}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${data.task}</h5>
-    <p class="card-text">${data.about}.</p>
-    <hr>
-    <div class="priority">
-    <p><i class='fa fa-warning'></i> Priority level<a class="btn-clicks btn btn-success"><span class="result">${data.priority}</span></a></p>
-    <p><i class='fa fa-calendar-times-o'></i> Deadline: ${data.deadline}</p>
-    <hr>
-    </div>
-    <div class="buttons">
-    <button type="button" class="btn btn-danger"><i class='fa fa-trash'></i> Delete</button>
-    <button type="button" class="btn btn-success"><i class='fa fa-check-circle'></i> Done</button>
-    </div>
-  </div>
-</div>
 </div>
     `;
+
+            priorityAction();
+        }
+        if (data.priority >= 2 && data.priority <= 3) {
+            document.getElementById("tasklist").innerHTML += `
+
+<div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+    <div class="card" ">
+        <div class="taskBtn">
+            <button type="button" class="btn btn-info btn-sm text-white">Task</button>
+            <p><i class='fa fa-bookmark-o'></i>&nbsp<i class='fa fa-ellipsis-v'></i></p>
+        </div>
+            <img src="${data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${data.task}</h5>
+                <p class="card-text">${data.about}.</p>
+                <hr>
+                <div class="priority">
+                <p><i class='fa fa-warning'></i> Priority level<a class="btn-clicks btn btn-warning"><span class="result">${data.priority}</span></a></p>
+                <p><i class='fa fa-calendar-times-o'></i> Deadline: ${data.deadline}</p>
+                <hr>
+                </div>
+                <div class="buttons">
+                    <button type="button" class="btn btn-danger"><i class='fa fa-trash'></i> Delete</button>
+                    <button type="button" class="btn btn-success"><i class='fa fa-check-circle'></i> Done</button>
+                </div>
+            </div>
+    </div>
+</div>
+    `;
+
+            priorityAction();
+        }
+
+        if (data.priority >= 4) {
+            document.getElementById("tasklist").innerHTML += `
+
+<div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+<div class="card" ">
+    <div class="taskBtn">
+        <button type="button" class="btn btn-info btn-sm text-white">Task</button>
+        <p><i class='fa fa-bookmark-o'></i>&nbsp<i class='fa fa-ellipsis-v'></i></p>
+    </div>
+        <img src="${data.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${data.task}</h5>
+            <p class="card-text">${data.about}.</p>
+            <hr>
+            <div class="priority">
+            <p><i class='fa fa-warning'></i> Priority level<a class="btn-clicks btn btn-danger"><span class="result">${data.priority}</span></a></p>
+            <p><i class='fa fa-calendar-times-o'></i> Deadline: ${data.deadline}</p>
+            <hr>
+            </div>
+            <div class="buttons">
+                <button type="button" class="btn btn-danger"><i class='fa fa-trash'></i> Delete</button>
+                <button type="button" class="btn btn-success"><i class='fa fa-check-circle'></i> Done</button>
+            </div>
+        </div>
+</div>
+</div>
+`;
+
+            priorityAction();
+        }
     }
-    priorityAction();
 }
 
 function priorityAction() {
@@ -43,10 +107,13 @@ function priorityAction() {
 
 
             var sortedArray = plans.sort((a, b) => b.priority - a.priority);
+
             updateHTML(sortedArray);
+
         });
     }
 }
 
+
 updateHTML(plans);
-priorityAction();
+// priorityAction();
